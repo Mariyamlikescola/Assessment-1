8267fb8a143364d52365df91b04e257ca33fe246
Mariyam Naeem <mariyamnaeem2706@gmail.com>
2024-11-24 17:01:15 +0400

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,23 @@
+#writing a loop  that counts up from 0 to 50 in increments of 1.
+for i in range(51):
+    print(i)
+
+ #Writing a loop that counts down from 50 to 0 in decrements of 1.
+for i in range(50,-1,-1):
+    print(i)
+
+ #Writinh a loop that counts up from 30 to 50 in increments of 1.
+for i in range(30,51):
+    print(i)
+
+ #Writing a loop that counts down from 50 to 10 in decrements of 2.
+for i in range(50,9,-2):
+    print(i)
+
+ #Writing a loop that counts up from 100 to 200 in increments of 5.
+for i in range(100,201,+5):
+    print(i)
+
+    #WRITING A LOOP THAT COUNTS UP FROM 200 to 350 in increments of 5
+for i in range(200,351,+5):
+    print(i)
