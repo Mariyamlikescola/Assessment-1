a89ceb25ae4c80e43d316dd682117b52f1e81f51
Mariyam Naeem <mariyamnaeem2706@gmail.com>
2024-11-24 13:59:51 +0400

    Add files via upload

---
+++ sum of numbers.py
@@ -0,0 +1,8 @@
+#Declaring a variable and initializing it with the integer value 8.
+number1=8
+#Declaring the second variable and initializing it with the integer value 10.
+number2=10
+#Declaring a third variable that stores the sum of first two numbers.
+sum_of_numbers=number1+number2
+#Printing the value of the sum.
+print("The sum of", number1, "and", number2, "is:", sum_of_numbers)
