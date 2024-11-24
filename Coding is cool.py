93f0a622377e43e36f9b61745a0a53bdde4b4803
Mariyam Naeem <mariyamnaeem2706@gmail.com>
2024-11-24 13:38:21 +0400

    Coding is cool

---
+++ Coding is cool.py
@@ -0,0 +1,9 @@
+# Giving values to the variables
+word1="Coding"
+word2="is"
+word3="Cool"
+
+# Use string concatenation to combine the variables and print the phrase
+phrase= word1+ " "+ word2 +" "+ word3
+#Printing the phrase
+print(phrase)
